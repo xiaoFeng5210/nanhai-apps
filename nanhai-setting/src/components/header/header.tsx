97f621c5d3cde09f33114719128776b93194d9fe
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-02 15:56:37 +0800

    fix

---
+++ nanhai-setting/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss'
+
+function Header() {
+  return (
+    <div className="w-fit pt-2 pb-2">
+      <h1 className="text-4xl font-bold backdrop-text">南海系统控制</h1>
+    </div>
+  );
+}
+
+export default Header;
