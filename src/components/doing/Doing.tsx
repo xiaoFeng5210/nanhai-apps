adeb8bc97326c5d81077d3620e8f98bf18a44a74
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-05 15:18:02 +0800

    样式差不多完成

--- src/components/doing/Doing.tsx
+++ src/components/doing/Doing.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Doing = () => {
   return (
     <div className="flex-[3] flex flex-col font-serif">
-      <h1 className="text-8xl flex justify-center">准 备 中</h1>
+      <h1 className="text-8xl flex justify-center">制 作 中</h1>
       <h2 className="text-4xl flex justify-center">PREPARING</h2>
 
       <div className="flex-1 grid grid-cols-4 grid-rows-3">
